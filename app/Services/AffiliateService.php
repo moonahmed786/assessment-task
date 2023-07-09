1e4fabeb10f7b67f0d78beb122a6d2530a850697
moonahmed786 <moonahmed786@gmail.com>
2023-07-10 02:26:36 +0500

    Change in data

--- app/Services/AffiliateService.php
+++ app/Services/AffiliateService.php
@@ -31,10 +31,9 @@ class AffiliateService
      */
     public function register(Merchant $merchant, string $email, string $name, float $commissionRate): Affiliate
     {
-        // TODO: Complete this method
-        try {    
+        // TODO: Complete this method  
                 $user = new User();
-                
+
                 $data = [
                     'name' => $name,
                     'email' => $email,
@@ -46,40 +45,24 @@ class AffiliateService
                     'email' => 'required|email|max:255|unique:users',
                     'commissionRate' => 'required|numeric'
                 ]);
+                // dd($validator);
                 if ($validator->fails() == true) {
-                    return response()->json([
-                        'message' => 'Data has some error please check.',
-                    ], 500);
+                    throw new AffiliateCreateException('Data has some error please check.');
                 }
-
                 
-                $user->name = $data['name'];
-                $user->email = $data['email'];
-                $user->password = Hash::make('password'); // Remember to hash the password
-                $user->type = User::TYPE_AFFILIATE; // Set the user type according to your constants
-                if($user->save()){
-                    // \DB::connection()->enableQueryLog();
-                    // Register a new merchant
-                    // dd($merchant);
-                    $affiliate = new Affiliate();
-                    $affiliate->user_id = $user->id;
-                    $affiliate->merchant_id = $merchant->id;
-                    $affiliate->commission_rate = $commissionRate;
-                    $affiliate->discount_code = ""; // Set the merchant default commission rate if you want then change it here'
-                    $affiliate->save();
-                    // $queries = \DB::getQueryLog();
-                    // dd($queries);
-                    return $affiliate;
-                    // dd($affiliate);
-                }
-
-        } catch (\Exception $e) {
-            // Handle the exception
-            dd($e->getMessage());
-            return response()->json([
-                'message' => 'Affiliate registered successfully.',
-                'error' => $e->getMessage(),
-            ], 500);
-        }
+                    $user->name = $data['name'];
+                    $user->email = $data['email'];
+                    $user->password = Hash::make('password'); // Remember to hash the password
+                    $user->type = User::TYPE_AFFILIATE; // Set the user type according to your constants
+                    if($user->save()){
+                        $affiliate = new Affiliate();
+                        $affiliate->user_id = $user->id;
+                        $affiliate->merchant_id = $merchant->id;
+                        $affiliate->commission_rate = $commissionRate;
+                        $affiliate->discount_code = ""; // Set the merchant default commission rate if you want then change it here'
+                        $affiliate->save();
+                        return $affiliate;
+                    }
+                
     }
 }
